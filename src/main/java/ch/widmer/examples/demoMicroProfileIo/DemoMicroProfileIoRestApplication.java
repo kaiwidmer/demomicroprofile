0e15578e2af703ea09c4b99d9d708a91d33eccd8
Kai <Kai@kais-macbookpro.home>
2019-09-17 09:55:44 +0200

    initial

---
+++ src/main/java/ch/widmer/examples/demoMicroProfileIo/DemoMicroProfileIoRestApplication.java
@@ -0,0 +1,11 @@
+package ch.widmer.examples.demoMicroProfileIo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class DemoMicroProfileIoRestApplication extends Application {
+}
---
+++ src/main/java/ch/widmer/examples/demoMicroProfileIo/health/ServiceHealthCheck.java
@@ -0,0 +1,19 @@
+package ch.widmer.examples.demoMicroProfileIo.health;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.health.Health;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+
+@Health
+@ApplicationScoped
+public class ServiceHealthCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+
+        return HealthCheckResponse.named(ServiceHealthCheck.class.getSimpleName()).up().build();
+
+    }
+}
